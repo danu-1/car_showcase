2129bb39a8c5cfff7d2758f58c4ff9ee54013716
danie Panta <admin@DESKTOP-LFCF3BJ>
2025-09-05 10:39:47 +0300

    Srcgbar

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { Combobox,Transition } from '@headlessui/react';
+
 import { SearchManufacturerProps } from '@/types'
 
 const SearchManufacturer = ({manufacturer, setManufacturer }: SearchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className='search-manufacturer'>
+        <Combobox>
+            <div className='relative w-full'>
+
+            </div>
+        </Combobox>
+    </div>
   )
 }
 
